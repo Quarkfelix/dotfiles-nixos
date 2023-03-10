bbe49684233395f042f4dafef5ad904a3d52c565
marc <marc-felix.schmitz@protonmail.com>
2023-03-10 15:02:18 +0100

    fixed lag and slow terminal opening -> update drivers

---
+++ themes/orange/theme.bash
@@ -0,0 +1,88 @@
+# ------------------------------------------------------------------------------
+# Copyright (C) 2020-2022 Aditya Shakya <adi1090x@gmail.com>
+#
+# Hack Theme
+# ------------------------------------------------------------------------------
+
+
+# Colors
+background='#031b22'
+foreground='#839496'
+color0='#031b22' # black
+color1='#dc322f' # red
+color2='#e95508' # green
+color3='#b58900' # yellow
+color4='#268bd2' # blue
+color5='#d33682' # magenta
+color6='#2aa198' # cyan
+color7='#eee8d5' # white
+
+# bright colors
+color8='#002b36' # black
+color9='#cb4b16' # red
+color10='#e95508' # green
+color11='#657b83' # yellow
+color12='#839496' # blue
+color13='#6c71c4' # magenta
+color14='#93a1a1' # cyan
+color15='#fdf6e3' # white
+
+accent='#fd5c09'
+color_red='#ff0000'
+color_green='#00ff00'
+color_yellow='#ffff00'
+
+light_value='0.05'
+dark_value='0.30'
+
+# Wallpaper
+wallpaper="$HOME/.config/dotfiles-nixos/wallpapers/orange.jpg"
+
+# Polybar
+polybar_font='Iosevka Nerd Font:size=10;3'
+
+# Rofi
+rofi_font='Iosevka 10'
+rofi_icon='Papirus-Apps'
+
+# Terminal
+terminal_font_name='JetBrainsMono Nerd Font'
+terminal_font_size='10'
+
+# Geany
+geany_colors='hack.conf'
+geany_font='JetBrains Mono 10'
+
+# Appearance
+gtk_font='Noto Sans 9'
+gtk_theme='Hack'
+icon_theme='Hack'
+cursor_theme='LyraB'
+
+# Dunst
+dunst_width='300'
+dunst_height='80'
+dunst_offset='10x36'
+dunst_origin='top-right'
+dunst_font='Iosevka 10'
+dunst_border='1'
+dunst_separator='1'
+
+# Picom
+picom_backend='glx'
+picom_corner='0'
+picom_shadow_r='14'
+picom_shadow_o='0.30'
+picom_shadow_x='-12'
+picom_shadow_y='-12'
+picom_blur_method='none'
+picom_blur_strength='0'
+
+# Bspwm
+bspwm_fbc="$accent"
+bspwm_nbc="$background"
+bspwm_abc="$color5"
+bspwm_pfc="$color2"
+bspwm_border='1'
+bspwm_gap='10'
+bspwm_sratio='0.50'
